eb908e3a072eceb3c74a25b0002acea03c579cea
unknown <dmadrigal0696@gmail.com>
2021-04-18 19:27:38 -0600

    Update categoria.input.ts

--- API/src/resolvers/categoria/categoria.input.ts
+++ API/src/resolvers/categoria/categoria.input.ts
@@ -1,21 +1,20 @@
 import { InputType, Field, ID, Authorized } from "type-graphql";
 import { EstadosTypes } from "../../enum/estados.enum";
-import { Usuario } from "../../entities/usuario";
-import { Servicio } from "../../entities/servicio";
+import { ServicioInput } from "../servicio/servicio.input";
 
 
 @InputType({ description: "información de las categorias" })
 export class CategoriaInput {
 
-    @Field(() => Usuario)
-    codigo!: Usuario;
+    @Field(() => String)
+    codigo!: string;
 
-    @Field(()=> String)
-    descripcion!: string
-
-    @Field(() => Servicio)
-    servicio!: Servicio;
+    @Field(() => String)
+    descripcion!: string;
 
     @Field(type => EstadosTypes)
     estado!: EstadosTypes;
+
+    @Field(type => [ServicioInput])
+    servicios!: ServicioInput[];
 }
